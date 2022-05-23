b9f47880995f04f1981ec97860469324ba462ecf
44chan <philipp.dyat@gmail.com>
2022-05-23 16:35:07 -0400

    Initial commit

---
+++ src/test/java/CodingEX/Coding_2.java
@@ -0,0 +1,39 @@
+package CodingEX;
+
+public class Coding_2 {
+
+    public static void main(String[] args) {
+
+
+        long a = 0;
+        long b = 1;
+
+        for (int i = 0; i <4 ; i++) {
+
+            a = a+b;
+            b = a+ b;
+
+            System.out.println("a = " + a);
+            System.out.println("b = " + b);
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+    /*
+    2.  Write a function that returns the first 60 values in a Fibonacci sequence (as a list of numbers)
+First number is 0, second number is 1. Everything else is the sum of the previous two numbers
+0, 1, 1, 2, 3, 5, 8, …
+If they are unclear on what a Fibonacci sequence is:
+
+     */
+
+}
